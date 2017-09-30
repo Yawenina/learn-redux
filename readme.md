67ef6610ade5cb51796ac5532ef6e3ccac5e0861
xiaoyawen <xiaoyawen@finogeeks.com>
2017-09-30 15:20:58 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Learn Redux
+
+学习 `Redux` 的过程。通过这个简单的 TodoApp 理解。
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+    ],
+  },
+};
